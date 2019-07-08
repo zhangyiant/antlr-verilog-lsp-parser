01e7728b2a793ce0f0ea1f9047ec83f621cb3bd8
Yi Zhang <zhangyiant@hotmail.com>
2019-07-08 20:49:47 +0800

    Add package-lock.json.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,3 @@
-// import * as antlr4 from 'antlr4';
-// import * as VerilogLexer from './Verilog2001Lexer';
-// import * as VerilogParser from './Verilog2001Parser';
-// import * as VerilogListener from './Verilog2001Listener';
-// import * as fs from 'fs';
-
-// let input = fs.readFileSync("resources/a.v", { encoding: "utf8"});
-// let chars = new antlr4.InputStream(input);
-// let lexer: antlr4.Lexer = new VerilogLexer.Verilog2001Lexer(chars) as unknown as antlr4.Lexer;
-// let tokens = new antlr4.CommonTokenStream(lexer);
-// let parser = new VerilogParser.Verilog2001Parser(tokens);
-// let tree = parser.module_declaration();
-// let listener = new VerilogListener.Verilog2001Listener();
-
-
-// tree.accept()
-// class Visitor {
-//     visitChildren(ctx) {
-//         if (!ctx) {
-//             return;
-//         }
-//         if (ctx.children) {
-//             return ctx.children.map(child => {
-//                 if (child.children && child.children.length != 0) {
-//                     return child.accept(this);
-//                 } else {
-//                     return child.getText();
-//                 }
-//             })
-//         }
-//     }
-// }
-
-// tree.accept(new Visitor());
 var antlr4 = require('antlr4');
 var VerilogLexer = require('./Verilog2001Lexer').Verilog2001Lexer;
 var VerilogParser = require('./Verilog2001Parser').Verilog2001Parser;
